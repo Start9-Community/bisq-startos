fea73706aa28032d7bc2b25d071ae14d403a2e39
BeeJoe <124329396+BeeJoe@users.noreply.github.com>
2026-07-06 19:40:31 -0400

    Update current version to v1.10.3

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_1_10_2 } from './v1.10.2'
+import { v_1_10_3 } from './v1.10.3'
 
 export const versionGraph = VersionGraph.of({
-  current: v_1_10_2,
+  current: v_1_10_3,
   other: [],
 })
