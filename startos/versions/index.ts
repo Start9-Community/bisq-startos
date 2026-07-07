e5a083dec85c2dfb4f025803d3aeac433b36ee64
BeeJoe <124329396+BeeJoe@users.noreply.github.com>
2026-07-06 20:16:27 -0400

    Refactor versionGraph to use current version import

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_1_10_3 } from './v1.10.3'
+import { current } from './current'
 
 export const versionGraph = VersionGraph.of({
-  current: v_1_10_3,
+  current,
   other: [],
 })
